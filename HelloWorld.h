301084b153c6389a9fd1918edc1b1439d6ed2b5f
p-hantom <917978341@qq.com>
2018-03-10 17:49:50 +0800

    finished

--- HelloWorld.h
+++ HelloWorld.h
@@ -1,6 +1,14 @@
+#include <iostream>
 #ifndef _HELLO_WORLD_
 #define _HELLO_WORLD_
+//#include <iostream>
+using namespace std;
+
 class HelloWorld {
-  
+public: 
+  	HelloWorld(){
+  		cout<<"hello world"<<endl;
+  	} 
+  	~HelloWorld(){}
 };
 #endif
