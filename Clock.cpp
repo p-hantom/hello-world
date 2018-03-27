d12ced7ae6ee04ec4708c93b975ff95498f67850
p-hantom <32257691+p-hantom@users.noreply.github.com>
2018-03-27 09:02:56 +0800

    Add files via upload

--- Clock.cpp
+++ Clock.cpp
@@ -1,11 +1,64 @@
+#include <iostream>
+using namespace std;
+
 class Clock {
+	public:
+		Clock(){
+			s=0;
+			h=0;
+			m=0;		
+		}
+		~Clock(){
+		}
+		void tick(){
+			if(s<59)
+				s++;
+			else{
+				s=0;
+				if(m<59)
+					m++;
+				else{
+					m=0;
+					if(h<23)
+						h++;
+					else{
+						h=0;
+					}
+				}
+			}
+		}
+		void show(){
+			if(h<10)
+				cout<<"0"<<h<<":";
+			else
+				cout<<h<<":";
+			if(m<10)
+				cout<<"0"<<m<<":";
+			else
+				cout<<m<<":";
+			if(s<10)
+				cout<<"0"<<s<<endl;
+			else
+				cout<<s<<endl;
+		}
+		void adjust(int hour1,int minute1,int second1){
+			if(hour1>=24||minute1>=60||second1>=60){
+				cout<<"wrong adjustment"<<endl;
+			}
+			h=hour1;
+			m=minute1;
+			s=second1;
+		}
+	private:
+		int h,m,s;
+		
 };
 
 int main() {
   Clock ck;
-  ck.tick;
+  ck.tick();
   ck.show();
-  ck.adjust(16,45,00);
+  ck.adjust(16,45,0);
   ck.show();
   return 0;
 }
--- Date.cpp
+++ Date.cpp
@@ -1,8 +1,13 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
 class Date {
 public:
   Date(int y, int m, int d);
   ~Date();
-
+	
+  int max();
   void nextDay();
   void previousDay();
   bool equal(Date other);
@@ -12,6 +17,73 @@ private:
   int month;
   int day;
 };
+
+Date::Date(int y, int m, int d):year(y),month(m),day(d){
+}
+
+Date::~Date(){
+}
+
+int Date::max(){
+	if(month==2){
+		if(year%4==0&&year%100!=0||year%400==0){
+			return 29;
+		}
+		else 
+			return 28;
+	}
+	if(month==1||month==3||month==5||month==7||month==8||month==10||month==12)
+		return 31;
+	if(month==4||month==6||month==9||month==11)
+		return 30;
+}
+
+void Date::nextDay(){
+	if(day<max()){
+		day++;
+		return;
+	}
+	if(day=max()){
+		if(month<12){
+			month++;
+			day=1;
+		}
+		else{
+			month=1;
+			year++;
+			day=1;
+		}
+		return;
+	}
+}
+
+void Date::previousDay(){
+	if(day==1){
+		if(month==1){
+			year--;
+			month=12;
+			day=31;
+		}
+		else{
+			month--;
+			day=max();
+		}
+		return;
+	}
+	day--;
+	return;
+}
+
+void Date::show(){
+	cout<<year<<","<<month<<","<<day<<endl;
+}
+
+bool Date::equal(Date other){
+	if(other.year==this->year&&other.month==this->month&&other.day==this->day){
+		return true;
+	}
+	return false;
+}
 
 int main() {
   Date today(2018,3,20);
