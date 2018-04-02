1d4292214d1de679fc7a69d96d40047343953dcd
p-hantom <32257691+p-hantom@users.noreply.github.com>
2018-04-02 18:45:54 +0900

    Add files via upload

--- Prime.c
+++ Prime.c
@@ -1,21 +1,51 @@
-/*满足下列条件的自然数称为超级素数:该数本身,所有数字之和,所有数字之积以及所有数字的平方和都是素数.例如113就是一个超级素数.求[100,9999]之内:(1)超级素数的个数.(2)所有超级素数之和.(3)最大的超级素数.*/
+#include <stdio.h>
+
 int sum_bit(int num) {
-  return 0;
+	int temp=num;
+	int sum=0;
+	while(temp!=0){
+		int t=temp%10;
+		temp/=10;
+		sum+=t;
+	}
+  return sum;
 }
 
 int multi_bit(int num) {
-  return 0;
+	int temp=num;
+	int sum=1;
+	while(temp!=0){
+		int t=temp%10;
+		temp/=10;
+		sum*=t;
+	}
+  return sum;
 }
 
 int square_sum_bit(int num) {
-  return 0;
+	int temp=num;
+	int sum=0;
+	while(temp!=0){
+		int t=temp%10;
+		temp/=10;
+		sum+=t*t;
+	}
+  return sum;
 }
 
-bool isprime(int num) {
-  return false;
+int isprime(int num) {
+	int i;
+  for(i=2;i<num;i++){
+		if(num%i==0){
+			return 0;
+		}
+	}
+	return 1;
 }
 
 int main() {
-  if(isprime(113)&&isprime(sum_bit(113))&&isprime(multi_bit(113))&&isprime(square_sum_bit(113)))
+  if(isprime(113)&&isprime(sum_bit(113))&&isprime(multi_bit(113))&&isprime(square_sum_bit(113))){
+  	printf("it's a super prime\n");
+  }
     //to do sth
 }
--- Prime.cpp
+++ Prime.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
 class Prime {
 public:
   Prime(int num);
@@ -9,15 +13,68 @@ public:
   Prime multiBit();
   Prime sqaureSumBit();
 private:
-  const int mNum;
+  const int m;
 };
+
+Prime::Prime(int num):m(num) {
+}
+
+Prime::~Prime(){
+} 
+
+bool Prime::isPrime(){
+	for(int i=2;i<m;i++){
+		if(m%i==0){
+			return false;
+		}
+	}
+	return true;
+}
+
+Prime Prime::sumBit(){
+	int temp=m;
+	int sum=0;
+	while(temp!=0){
+		int t=temp%10;
+		temp/=10;
+		sum+=t;
+	}
+	Prime p(sum);
+	return p;
+}
+
+Prime Prime::multiBit(){
+	int temp=m;
+	int sum=1;
+	while(temp!=0){
+		int t=temp%10;
+		temp/=10;
+		sum*=t;
+	}
+	Prime p(sum);
+	return p;
+}
+
+Prime Prime::sqaureSumBit(){
+	int temp=m;
+	int sum=0;
+	while(temp!=0){
+		int t=temp%10;
+		temp/=10;
+		sum+=t*t;
+	}
+	Prime p(sum);
+	return p;
+}
 
 int main() {
   Prime pm(113);
   Prime sb = pm.sumBit();
   Prime mb = pm.multiBit();
   Prime ssb = pm.sqaureSumBit();
-  if(pm.isPrime() && sb.isPrime() && mb.isPrime() && ssb.isPrime())
-    ; //to do sth
+  if(pm.isPrime() && sb.isPrime() && mb.isPrime() && ssb.isPrime()){
+  	cout<<"it's a super prime."<<endl;
+  }
+
   return 0;
 }
